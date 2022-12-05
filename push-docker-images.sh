0ec999ae41e97d883ce5b649ffe4f2a5031c934e
Antoine Cailliau <antoinecailliau@gmail.com>
2022-12-05 09:33:03 +0100

    Updates build script

--- push-docker-images.sh
+++ push-docker-images.sh
@@ -1,67 +1,91 @@
-if [ -z $1 ]; then 
-  echo "Usage:";
-  echo "./push-docker-images.sh v2.1 v2.1.1";
-  exit 1;
-fi
-
-if [ -z $2 ]; then 
-  echo "Usage:";
-  echo "./push-docker-images.sh v2.1 v2.1.1";
-  exit 1;
-fi
-
 MAJOR=$1
 MINOR=$2
 
 echo "📦 Pushing DocIntel.Services.DocumentAnalyzer"
 docker push docintelapp/document-analyzer:latest
-docker push docintelapp/document-analyzer:$MAJOR
-docker push docintelapp/document-analyzer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/document-analyzer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/document-analyzer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.DocumentIndexer"
 docker push docintelapp/document-indexer:latest
-docker push docintelapp/document-indexer:$MAJOR
-docker push docintelapp/document-indexer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/document-indexer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/document-indexer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.Importer"
 docker push docintelapp/importer:latest
-docker push docintelapp/importer:$MAJOR
-docker push docintelapp/importer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/importer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/importer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.Newsletters"
 docker push docintelapp/newsletter:latest
-docker push docintelapp/newsletter:$MAJOR
-docker push docintelapp/newsletter:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/newsletter:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/newsletter:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.Scraper"
 docker push docintelapp/scraper:latest
-docker push docintelapp/scraper:$MAJOR
-docker push docintelapp/scraper:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/scraper:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/scraper:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.SourceIndexer"
 docker push docintelapp/source-indexer:latest
-docker push docintelapp/source-indexer:$MAJOR
-docker push docintelapp/source-indexer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/source-indexer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/source-indexer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.TagIndexer"
 docker push docintelapp/tag-indexer:latest
-docker push docintelapp/tag-indexer:$MAJOR
-docker push docintelapp/tag-indexer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/tag-indexer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/tag-indexer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.Services.Thumbnailer"
 docker push docintelapp/thumbnailer:latest
-docker push docintelapp/thumbnailer:$MAJOR
-docker push docintelapp/thumbnailer:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/thumbnailer:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/thumbnailer:$MINOR
+fi
 
 echo ""
 echo "📦 Pushing DocIntel.WebApp"
 docker push docintelapp/webapp:latest
-docker push docintelapp/webapp:$MAJOR
-docker push docintelapp/webapp:$MINOR
+if [ -z "$MAJOR" ]; then 
+  docker push docintelapp/webapp:$MAJOR
+fi
+if [ -z "$MINOR" ]; then 
+  docker push docintelapp/webapp:$MINOR
+fi
