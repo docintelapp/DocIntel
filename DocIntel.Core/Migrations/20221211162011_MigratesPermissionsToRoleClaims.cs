a352069c3841f4f13e6c1b41bfe10a94f28bc49c
Antoine Cailliau <antoinecailliau@gmail.com>
2022-12-13 08:32:04 +0100

    Adds missing migration files

---
+++ DocIntel.Core/Migrations/20221211162011_MigratesPermissionsToRoleClaims.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocIntel.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigratesPermissionsToRoleClaims : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            INSERT INTO "AspNetRoleClaims" ("RoleId", "ClaimType", "ClaimValue") 
+            (SELECT "Id" AS "RoleId", 'docintel.permission' AS "ClaimType", "ClaimValue" 
+             FROM "AspNetRoles", unnest(string_to_array("PermissionList", ',')) "ClaimValue" 
+             EXCEPT 
+             SELECT "RoleId", "ClaimType", "ClaimValue" FROM "AspNetRoleClaims"); 
+            """);
+        
+            migrationBuilder.DropColumn(
+                name: "PermissionList",
+                table: "AspNetRoles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PermissionList",
+                table: "AspNetRoles",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
